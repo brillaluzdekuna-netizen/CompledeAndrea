b4dd23dd7de2ad1bf13d42e0dedf5b176ce5eefb
andreit <brillaluzdekuna@gmail.com>
2025-11-29 02:39:11 -0500

    Fix Flask app to bind to 0.0.0.0 for Render deployment

--- 1ACC0184_TB2_U202310988_Santur_Andrea_Actualizado/app.py
+++ 1ACC0184_TB2_U202310988_Santur_Andrea_Actualizado/app.py
@@ -432,5 +432,4 @@ if __name__ == '__main__':
         # ignore if complex_grafo not available
         pass
 
-    app.run(host='127.0.0.1', port=5000, debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=False)
